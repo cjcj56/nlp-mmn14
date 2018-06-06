7e32de7ff5c70a83745407f11883190792af6e84
Binyamin <cj56@walla.co.il>
2018-06-06 20:42:51 +0300

    Auto stash before merge of "master" and "origin/master"
    aligning

---
+++ evalb/run_evals - Copy.bat
@@ -0,0 +1,4 @@
+evalb -p new.prm ../data/heb-ctrees.gold ../exps/test_-1.parsed > ../exps/test_-1.eval
+evalb -p new.prm ../data/heb-ctrees.gold ../exps/test_0.parsed > ../exps/test_0.eval
+evalb -p new.prm ../data/heb-ctrees.gold ../exps/test_1.parsed > ../exps/test_1.eval
+evalb -p new.prm ../data/heb-ctrees.gold ../exps/test_2.parsed > ../exps/test_2.eval
