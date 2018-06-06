8a1e30a7fddb421ef2fa3f628cb1d00a1dfa6aaf
Binyamin <cj56@walla.co.il>
2018-06-06 20:42:10 +0300

    aligning

---
+++ evalb/run_evals.bat
@@ -0,0 +1,4 @@
+evalb -p new.prm ../data/heb-ctrees.gold ../exps/test_-1_h-1.parsed > ../exps/test_-1_h-1.eval
+evalb -p new.prm ../data/heb-ctrees.gold ../exps/test_0_h0.parsed > ../exps/test_0_h0.eval
+evalb -p new.prm ../data/heb-ctrees.gold ../exps/test_1_h1.parsed > ../exps/test_1_h1.eval
+evalb -p new.prm ../data/heb-ctrees.gold ../exps/test_2_h2.parsed > ../exps/test_2_h2.eval
